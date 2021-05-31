4d375ba01d878de48a333976343c318c32f84b87
vv_tomilin <backatom1@gmail.com>
2021-05-31 14:10:33 +0500

    Урок 109. Начинаем создавать новое приложение

---
+++ src/components/resto-service-context/resto-service-context.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RestoreServiceContext = React.createContext();
+
+export default RestoreServiceContext;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import ErrorBoundry from './components/error-boundry';
+import RestoServise from './services/resto-service';
+//!import RestoServiseContext from './components/resto-service-context';
+import store from './store';
+
 import App from './components/app';
 
 import './index.scss';
+import RestoServiceContext from './components/resto-service-context';
 
+const restoService = new RestoServise();
 
 ReactDOM.render(
-    <App/>
+    <Provider store={store}>
+        <ErrorBoundry>
+            <RestoServiceContext.Provider value={restoService}>
+                <Router>
+                    <App />
+                </Router>
+            </RestoServiceContext.Provider>
+        </ErrorBoundry>
+    </Provider>
     , document.getElementById('root'));
 
